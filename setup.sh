71b71178722891854778641d0ec69f0da0941092
mute1997 <mute1008@gmail.com>
2016-12-09 06:00:47 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir install-sh
+cd install-sh
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/install.sh
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/chroot_setup.sh
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/chroot_setup.sh
+
+mkdir setting
+cd setting
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/setting/user.txt
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/setting/mirrorlist
+wget https://raw.githubusercontent.com/mute1997/archlinuxInstaller-sh/master/setting/netctl_setting
+
+cd ..
+chmod +x install.sh
+./instal.sh
